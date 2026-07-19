3fdbd386c86a1d071aa083acf4751a20a4f8145d
ge96juy <ge96juy@mytum.de>
2026-07-19 22:00:14 +0200

    Start Importing PsychoPy, clean up the LS processing, Randomization_order as reference instead of Balance Order

--- scripts/Processing.R
+++ scripts/Processing.R
@@ -4,24 +4,27 @@
 # Created: 19.07.2026
   # Last modified: 19.07.2026
 
-# Create reference for subject IDs ----
-trialorder <- import("data/raw/Boredom Balance TrialOrder.xlsx")
-#delete stray value at end of table and shrink to actual values
-trialorder[dim(trialorder)[1],dim(trialorder)[2]] <- NA
-trialorder <- trialorder [rowSums(!is.na(trialorder)) > 0,
-  colSums(!is.na(trialorder)) > 0] 
+# Create reference for subject IDs ====
+# Import subject documentation used during project
+randomization_order <- import("data/raw/Randomization Order.csv")
+
+# Manually remove the subject, where two out of three measures failed based on documentation
+randomization_order <- randomization_order[randomization_order$`Participant ID` != "DO13UE03",]
+
+# Fix missing or misspelled subject IDs and remove Umlaute based on IDs used in PsychoPy
+randomization_order[randomization_order$`Participant ID` == "D011ÜB30", "Participant ID"] <- "DO11UB30"
+
 
 # Limesurvey ====
+# Import the raw file and create a copy to process
 ls_raw <- import("data/raw/20260713_results-survey665152.csv")
 ls_proc <- ls_raw
 
 # Fix missing and missspelled subject IDs
 ls_proc[ls_proc$Versuchspersonencode == "AN", "Versuchspersonencode"] <- "AN07IE11"
-trialorder[trialorder$`Subject ID` == "DI16UB31", "Subject ID"] <- "DI16ÜB31"
-trialorder[trialorder$`Subject ID` == "D011ÜB30", "Subject ID"] <- "DO11ÜB30"
 
 # Filter limesurvey data for the subject IDs in trialorder to reduce to only the subjects included
-IDs <- c(trialorder$`Subject ID`)
+IDs <- c(randomization_order$`Participant ID`)
 ls_proc <- ls_proc[ls_proc$Versuchspersonencode %in% IDs,]
 
 # Save processed limesurvey data to processed folder
--- scripts/PsychoPyTest.R
+++ scripts/PsychoPyTest.R
@@ -1,28 +1,71 @@
 
 
-filenames <- list.files("raw/psychopy", pattern="*.csv")
+filenames <- list.files("data/raw/psychopy", pattern = "\\.csv$")
 
 df <- data.frame(filenames)
-# initialise empty columns for details
+rename(df, "Filename"="filenames")
 
 for (fname in filenames){
 
-	fname_list <- str_split(fname, "_")
+	fname_list <- strsplit(fname, "_")
 
-	df[fname, "Versuchspersonencode"] = fname_list[1]
+	df[df$filename == fname, "Subject ID"] <- fname_list[[1]][1]
 
-	if (fname_list[2] == "A" | fname_list[2] == "B"){
-		df[df$filename == fname, "A/B"] = fname_list[2]
-		df[df$filename == fname, "LB/HB"] = fname_list[3]
-		df[df$filename == fname, "Date"] = fname_list[8]
-		df[df$filename == fname, "Time"] = fname_list[9]
+	if (fname_list[[1]][2] == "A" | fname_list[[1]][2] == "B"){
+		df[df$filename == fname, "A/B"] <- fname_list[[1]][2]
+		df[df$filename == fname, "LB/HB"] <- fname_list[[1]][3]
+		df[df$filename == fname, "Date"] <- fname_list[[1]][8]
+		df[df$filename == fname, "Time"] <- fname_list[[1]][9]
+	}
+	else if (fname_list[[1]][2] == "LB" | fname_list[[1]][2] == "HB"){
+		df[df$filename == fname, "A/B"] <- fname_list[[1]][3]
+		df[df$filename == fname, "LB/HB"] <- fname_list[[1]][2]
+		df[df$filename == fname, "Date"] <- fname_list[[1]][8]
+		df[df$filename == fname, "Time"] <- fname_list[[1]][9]
 	}
 	else{
-		df[df$filename == fname, "A/B"] = fname_list[5]
-		df[df$filename == fname, "LB/HB"] = NA
-		df[df$filename == fname, "Date"] = fname_list[6]
-		df[df$filename == fname, "Time"] = fname_list[7]
+		df[df$filename == fname, "A/B"] <- fname_list[[1]][5]
+		df[df$filename == fname, "LB/HB"] <- NA
+		df[df$filename == fname, "Date"] <- fname_list[[1]][6]
+		df[df$filename == fname, "Time"] <- fname_list[[1]][7]
 	}
 }
 
+# Check if needs some renaming, don't think so
+
+# Join dataframe with condition from randomization_order
+df <- df %>% 
+  left_join(randomization_order %>% select("Participant ID", "order/condition"),
+  by = c("Subject ID" = "Participant ID"))
+
+# Reduce to the subjects actually measured
+df <- df[df$`Subject ID` %in% IDs,]
+
+# Manually exclude rows that should not be included based on documentation in randomization_order
+id_x <- which(df$`Subject ID` == "AN06AN18")[1]
+id_y <- which(df$`Subject ID` == "CA04TU11")[2]
+df <- df[-c(id_x,id_y),]
+remove (id_x, id_y)
+
+# Fill in LB/HB from that other file for all entries where the filename doesn't contain LB/HB
+df$`LB/HB`[is.na(df$`LB/HB`)] <- 
+	ifelse(
+		df$`A/B`[is.na(df$`LB/HB`)] == "A",
+		sub("_.*","", df$`order/condition`[is.na(df$`LB/HB`)]),
+		sub(".*_","", df$`order/condition`[is.na(df$`LB/HB`)])
+	)
+
 # Start reading in the actual data from the files and put it in new columns
+
+
+
+
+
+
+
+
+
+
+
+
+# Now sort this into processing.R
